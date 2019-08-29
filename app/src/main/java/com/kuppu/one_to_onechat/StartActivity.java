413b9f13ef76ebc69c61112c87cbaf5978d6d166
kuppusamy <kuppusamykaliyappan@gmail.com>
2019-08-29 16:20:13 +0530

    login and Registration completed

--- app/src/main/java/com/kuppu/one_to_onechat/StartActivity.java
+++ app/src/main/java/com/kuppu/one_to_onechat/StartActivity.java
@@ -1,14 +1,35 @@
 package com.kuppu.one_to_onechat;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class StartActivity extends AppCompatActivity {
 
-
+private Button login ,register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
+
+        login=findViewById(R.id.login);
+        register=findViewById(R.id.register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            }
+        });
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),RegisterActivity.class));
+            }
+        });
     }
 }
